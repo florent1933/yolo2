04581e7fe6417d6845eec537faf917b6ee673713
🚀 <clapie.florent@gmail.com>
2023-10-06 16:23:22 +0200

    u

--- src/app/feat/rxjs/step2/step2.component.ts
+++ src/app/feat/rxjs/step2/step2.component.ts
@@ -7,6 +7,17 @@ interface ORDER {
   date: Date;
 }
 
+interface ORDER_SUMMARY {
+  nbOfChocolatines: number;
+  moenyChocolatines: number;
+  nbOfCroissants: number;
+  moneyCroissants: number;
+  date: Date;
+}
+
+const CHOCOLATINE_PRICE = 1.3;
+const CROISSANT_PRICE = 1.1;
+
 const CHOLATINES_PRODUCTION = 3;
 const CHOCOLATINE_FACTORY_PRICE = 0.2;
 const CROISSANTS_FACTORY_PRICE = 0.16;
@@ -52,11 +63,51 @@ export class Step2Component implements OnInit {
     })
   );
 
+  moneys$: Observable<ORDER_SUMMARY> = this.orders$.pipe(
+    map((orders) => {
+      let newOrder: ORDER_SUMMARY = {
+        nbOfChocolatines: 0,
+        moenyChocolatines: 0,
+        nbOfCroissants: 0,
+        moneyCroissants: 0,
+        date: new Date(),
+      };
+
+      orders.forEach((order) => {
+        newOrder.nbOfChocolatines =
+          newOrder.nbOfChocolatines + order.nbOfChocolatines;
+        newOrder.nbOfCroissants =
+          newOrder.nbOfCroissants + order.nbOfCroissants;
+      });
+
+      return {
+        ...newOrder,
+        moenyChocolatines:
+          newOrder.nbOfChocolatines * CHOCOLATINE_PRICE +
+          newOrder.moenyChocolatines,
+        moneyCroissants:
+          newOrder.nbOfCroissants * CROISSANT_PRICE + newOrder.moneyCroissants,
+      };
+    })
+  );
+
+  stocks$: Observable<{
+    chocolatines: number;
+    croissants: number;
+  }> = combineLatest([this.chocolatines$, this.croissants$, this.moneys$]).pipe(
+    map(([chocolatines, croissants, money]) => {
+      return {
+        chocolatines: chocolatines - money.nbOfChocolatines,
+        croissants: croissants - money.nbOfCroissants,
+      };
+    })
+  );
+
   constructor() {}
 
   ngOnInit(): void {}
 
   getRandomNumber(min: number, max: number): number {
-    return Math.floor(Math.random() * (max + 1)) + min;
+    return Math.floor(Math.random() * max) + min;
   }
 }
